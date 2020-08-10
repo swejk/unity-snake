1a71e8e18ecb05671c92e8be3eed5cb7f0465bdb
Unknown <ulicny.martin000@gmail.com>
2020-08-10 18:34:50 +0200

    2 Basics - Scenes, GameObjects, Components
    - scene setup with simple player movement

---
+++ Assets/Scripts/Snake.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Snake : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            transform.position += Vector3.up;
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            transform.position += Vector3.down;
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.position += Vector3.right;
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.position += Vector3.left;
+        }
+    }
+}
