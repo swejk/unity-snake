1ae40da3463630a66f37845d1338488befb5d06f
Unknown <ulicny.martin000@gmail.com>
2020-08-17 19:15:22 +0200

    7 Game Build
    - build settings added
    - added button for exiting game

---
+++ Assets/Scripts/ExitGame.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class ExitGame : MonoBehaviour
+{
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Exit();
+        }
+    }
+}
